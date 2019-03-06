cd72fad5497babeef186d05d919c37c13a651940
Paul Williamsen <pwilliamsen@me.com>
2019-03-06 14:13:14 -0800

    portfolio work

--- src/models.py
+++ src/models.py
@@ -11,6 +11,7 @@ class Company(db.Model):
     __tablename__ = 'companies'
 
     id = db.Column(db.Integer, primary_key=True)
+    portfolio_id = db.Column(db.ForeignKey('portfolio.id'), nullable=False)
     name = db.Column(db.String(256), index=True, unique=True)
     symbol = db.Column(db.String(256), index=True, unique=True)
 
@@ -19,13 +20,13 @@ class Company(db.Model):
         return '<Company {}-{}>'.format(self.name, self.symbol)
 
 class Portfolio(db.Model):
-    __tablename__ = 'categories'
+    __tablename__ = 'portfolios'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(256), index=True)
+    portfolio_name = db.Column(db.String(256), index=True)
 
-    company = db.relationship('Company', backref='category', lazy=True)
+    company = db.relationship('Company', backref='portfolio', lazy=True)
 
 
     def __repr__(self):
-        return '<Category {}>'.format(self.name)
+        return '<Portfolio {}>'.format(self.portfolio_name)
